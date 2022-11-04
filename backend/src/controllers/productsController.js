8a4dd2a2ceef688730ee3dc32de1991adaf34d90
Luis Mera <luismera86@gmail.com>
2022-11-04 12:28:05 -0300

    Backend: agregado la verificación de id de mongo

--- backend/src/controllers/productsController.js
+++ backend/src/controllers/productsController.js
@@ -1,6 +1,9 @@
+/* eslint-disable camelcase */
+
 import { request, response } from 'express'
 
 import Product from '../models/productsModel.js'
+import { isValidObjectId } from 'mongoose'
 import logger from '../utils/logger.js'
 
 export const getProducts = async (req = request, res = response) => {
@@ -16,7 +19,13 @@ export const getProducts = async (req = request, res = response) => {
 export const getProductById = async (req = request, res = response) => {
   try {
     const { id } = req.params
-    const product = await Product.findById({ _id: id })
+    const isMongoId = isValidObjectId(id)
+    console.log(isMongoId)
+    if (!isMongoId) {
+      return res.status(400).json({ msg: 'No es un id válido' })
+    }
+    const product = await Product.findOne({ _id: id })
+
     res.status(200).json({ product })
   } catch (error) {
     logger.info('error', error)
@@ -45,9 +54,32 @@ export const createProduct = async (req = request, res = response) => {
 
 export const updateProduct = async (req = request, res = response) => {
   try {
-    const { id } = req.body
-    const { name, price, description, thumbnail } = req.body
-    const product = await Product.findByIdAndUpdate({ _id: id }, { name, price, description, thumbnail })
+    const { id } = req.params
+    const isMongoId = isValidObjectId(id)
+    console.log(isMongoId)
+    if (!isMongoId) {
+      return res.status(400).json({ msg: 'No es un id válido' })
+    }
+    const { name, price, description, thumbnail, category, quantity } = req.body
+    const product = await Product.findById({ _id: id })
+    await product.update({ name, price, description, thumbnail, category, quantity })
+    res.status(200).json({ msg: 'Producto modificado con éxito' })
+  } catch (error) {
+    logger.info('error', error)
+    res.status(404).json({ message: error.message })
+  }
+}
+
+export const updateProductQuantity = async (req = request, res = response) => {
+  try {
+    const { id_product } = req.params
+    const { quantity } = req.body
+    const isMongoId = isValidObjectId(id_product)
+    console.log(isMongoId)
+    if (!isMongoId) {
+      return res.status(400).json({ msg: 'No es un id válido' })
+    }
+    const product = await Product.findByIdAndUpdate({ _id: id_product }, { quantity })
     res.status(200).json({ product })
   } catch (error) {
     logger.info('error', error)
@@ -57,9 +89,15 @@ export const updateProduct = async (req = request, res = response) => {
 
 export const deleteProduct = async (req = request, res = response) => {
   try {
-    const { id } = req.body
-    const product = await Product.findByIdAndDelete({ _id: id })
-    res.status(200).json({ product })
+    const { id } = req.params
+    const isMongoId = isValidObjectId(id)
+    console.log(isMongoId)
+    if (!isMongoId) {
+      return res.status(400).json({ msg: 'No es un id válido' })
+    }
+    const product = await Product.findOne({ _id: id })
+    await product.remove()
+    res.status(200).json({ msg: 'Producto eliminado con éxito' })
   } catch (error) {
     logger.info('error', error)
     res.status(404).json({ message: error.message })
--- backend/src/routes/productsRouter.js
+++ backend/src/routes/productsRouter.js
@@ -1,13 +1,23 @@
-import { createProduct, deleteProduct, getProductById, getProducts, updateProduct } from '../controllers/productsController.js'
+import {
+  createProduct,
+  deleteProduct,
+  getProductById,
+  getProducts,
+  updateProduct,
+  updateProductQuantity,
+} from '../controllers/productsController.js'
 
 import { Router } from 'express'
+import { isAdminRole } from '../middlewares/validateRole.js'
+import { validateJwt } from '../middlewares/validateJwt.js'
 
 const productsRouter = Router()
 
 productsRouter.get('/', getProducts)
 productsRouter.get('/:id', getProductById)
-productsRouter.post('/', createProduct)
-productsRouter.put('/', updateProduct)
-productsRouter.delete('/', deleteProduct)
+productsRouter.post('/', validateJwt, isAdminRole, createProduct)
+productsRouter.put('/:id', validateJwt, isAdminRole, updateProduct)
+productsRouter.put('/:id_product', updateProductQuantity)
+productsRouter.delete('/:id', validateJwt, isAdminRole, deleteProduct)
 
 export default productsRouter
